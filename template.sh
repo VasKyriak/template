66667853659f385c2d9ed6fb5e47c8b7a5f5e5a0
Vasileios-Evripidis Kyriakidis <keftes86@gmail.com>
2017-02-21 23:02:47 +0100

    Add files via upload

---
+++ template.sh
@@ -0,0 +1,23 @@
+#create main folder
+mkdir projects
+
+#enter project
+cd projects
+
+mkdir my_project
+mkdir datasets
+mkdir general_scripts
+mkdir bin
+
+#enter my_project
+cd my_project
+
+mkdir scripts bash input ouput logs
+touch readme.txt
+touch commands.txt
+
+#enter bash and create bash files
+cd bash
+touch runall.sh
+touch filter.sh
+
